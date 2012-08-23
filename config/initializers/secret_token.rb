0b93e14af203b012076d6e80ecefb452fb894494
Cristian Rosu <redy_kdwa@yahoo.com>
2012-08-23 22:17:40 +0300

    Initial commit: Device & Oauth-Facebook authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectRedChambon::Application.config.secret_token = 'e0082c2fc09e10c48a06c22c96a56bd2e2656a812121025292ea14c6f0564a70d5581f3bf023820cd2f14ec2370c23583468fe5a207bf614a9c0079dcbb93fb5'
