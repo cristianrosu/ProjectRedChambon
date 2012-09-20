c5b6783508245f93a86146681ff9006c14a10c44
Cristian Rosu <redy_kdwa@yahoo.com>
2012-09-20 23:04:48 +0300

    admin panel begin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -9,4 +9,19 @@ class AdminController < ApplicationController
       format.json { render json: @events }
     end
   end
+
+  def new
+    @event = Event.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @event }
+    end
+  end
+
+  # GET /events/1/edit
+  def edit
+    @event = Event.find(params[:id])
+  end
+
 end
