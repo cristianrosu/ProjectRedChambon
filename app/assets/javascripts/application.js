87dd2c8fec37bb6aa57ddf069bef49785e36457b
Cristian Rosu <redy_kdwa@yahoo.com>
2012-10-03 17:16:03 +0300

    icons and stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,26 @@
         }
     });
 
+
+	/* Radial Menu */
+    function PieMenuInit(){   
+      $('#outer_container').PieMenu({
+        'starting_angel':$('#s_angle').val(),
+        'angel_difference' : $('#diff_angle').val(),
+        'radius':$('#radius').val(),
+      });     
+    }
+    $(function() {          
+      $("#submit_button").click(function() {reset(); }); 
+      // $( "#outer_container" ).draggable(); ..era draggable
+      PieMenuInit();
+      
+    });
+    function reset(){
+      if($(".menu_button").hasClass('btn-rotate'))
+      $(".menu_button").trigger('click');
+      
+      $("#info").fadeIn("slow").fadeOut("slow");
+      PieMenuInit();
+    }
+ 
